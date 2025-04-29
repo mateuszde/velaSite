b580b0be482bd0d7e0cd45a5bc6c882e12f53826
Mateusz <mat.demski@gmail.com>
2025-04-29 22:45:34 +0200

    Image comparision has been added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,11 @@ import AboutUs from "../../layout/AboutUs/AboutUs";
 import CallToAction from "../../layout/CallToAction/CallToAction";
 import Contact from "../../layout/Contact/Contact";
 
+import Container from "../../components/Container/Container";
+import ReactCompareImage from "react-compare-image";
+import before from "../../assets/Images/before.jpg";
+import after from "../../assets/Images/after.jpg";
+
 const Home = ({ dataCards }) => {
   return (
     <main>
@@ -13,6 +18,15 @@ const Home = ({ dataCards }) => {
       <Investment />
       <CallToAction />
       <AboutUs />
+      <Container>
+        <ReactCompareImage
+          leftImageCss={{ filter: "brightness(30%)" }}
+          leftImage={before}
+          sliderLineWidth={2}
+          rightImage={after}
+          sliderPositionPercentage={0.7}
+        />
+      </Container>
       <Contact />
     </main>
   );
